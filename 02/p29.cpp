7cefd948cb73123e0da497c8e5de4a9d213a5b99
Marco Villotta <gmarcovil@gmail.com>
2022-07-26 23:15:45 +0200

    end p29

--- 02/p29.cpp
+++ 02/p29.cpp
@@ -1,42 +1,58 @@
 #include <iostream>
 #include <string>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 int main()
 {
-  std::cout << "Please enter your first name: ";
+  // ask for the person's name
+  cout << "Please enter your first name: ";
 
-  std::string name;
-  std::cin >> name;
+  // read the name
+  string name;
+  cin >> name;
 
-  const std::string greeting = "Hello, " + name + "!";
+  // build the message that we intend to write
+  const string greeting = "Hello, " + name + "!";
 
   // the number of blanks surrounding the greeting
   const int pad = 1;
 
   // the number of rows and columns to write
   const int rows = pad * 2 + 3;
-  const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+  const string::size_type cols = greeting.size() + pad * 2 + 2;
     
   // separate the output from the input
-  std::cout << std::endl;
+  cout << endl;
 
   // write rows rows of input
-  int r = 0;
-  
   // invariant: we have written r rows so far
-  while (r != rows) {
+  for (int r = 0; r != rows; ++r) {
 
-    std::string::size_type c = 0;
+    string::size_type c = 0;
 
     // invariant: we have written c characters so far in the current row
     while (c != cols) {
-      // write one or more characters
-      // adjust the value of c to mantain the invariant
-    }
+
+      // is it time to write the greeting?
+      if (r == pad + 1 && c == pad + 1) {
+	cout << greeting;
+	c += greeting.size();
+      } else {
+
+	// are we on the border?
+	if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) 
+	  cout << "*";
+	else 
+	  cout << " ";
+	++c;
+	}
+      }
     
-    std::cout << std::endl;
-    ++r;
+      cout << endl;
+    }  
+    return 0;
   }
-  
-  return 0;
-}
